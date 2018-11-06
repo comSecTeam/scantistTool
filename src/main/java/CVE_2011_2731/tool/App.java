c86bc7ab4177ed68ba1600833c435dc4940cccf8
comSecTeam <44815259+comSecTeam@users.noreply.github.com>
2018-11-07 02:03:19 +0800

    Essentials

---
+++ src/main/java/CVE_2011_2731/tool/App.java
@@ -0,0 +1,13 @@
+package CVE_2011_2731.tool;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
